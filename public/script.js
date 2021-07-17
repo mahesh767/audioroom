1b707e7a19a6ff40bb83c1b16f41abd344350136
kalyan.b <kalyan.b@darwinbox.io>
2021-07-17 11:32:39 +0530

    host

--- public/script.js
+++ public/script.js
@@ -1,15 +1,13 @@
   const socket = io('/')
   const videoGrid = document.getElementById('video-grid')
   const myPeer = new Peer({
-    // config: {'iceServers': [
-    //   { url: 'stun:stun.l.google.com:19302' },
-    //   { url: 'turn:13.233.24.245:3478?transport=tcp', username : 'virtualcafe' , credential: 'virtualcafe' }
-    // ]},
-    // secure : true,
-    // host: '/virtualcafepeerjs.herokuapp.com',
-    // port: 443,
-    host : "/",
-    port : 3001,
+    config: {'iceServers': [
+      { url: 'stun:stun.l.google.com:19302' },
+      { url: 'turn::turn:52.66.48.157:3478?transport=tcp', username : 'virtualcafe' , credential: 'virtualcafe' }
+    ]},
+    secure : true,
+    host: '/virtualcafepeerjs.herokuapp.com',
+    port: 443,
   })
   let myVideoStream;
   const audio = createAudioNode()
