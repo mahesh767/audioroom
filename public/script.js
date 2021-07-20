27035711f9e02e8411c8f010bb3106d18f9b802b
kalyan.b <kalyan.b@darwinbox.io>
2021-07-20 19:44:27 +0530

    audio room

--- public/script.js
+++ public/script.js
@@ -2,8 +2,8 @@
   const videoGrid = document.getElementById('video-grid')
   const myPeer = new Peer({
     config: {'iceServers': [
-      { urls: 'turn:65.2.10.63?transport=tcp', username : 'virtualcafe' , credential: 'virtualcafe' },
-      { urls: 'turn:65.2.10.63?transport=udp', username : 'virtualcafe' , credential: 'virtualcafe' }
+      { urls: 'turn:65.2.33.97?transport=tcp', username : 'virtualcafe' , credential: 'virtualcafe' },
+      { urls: 'turn:65.2.33.97?transport=udp', username : 'virtualcafe' , credential: 'virtualcafe' }
     ]},
     secure : true,
     host : "virtualcafepeerjs.herokuapp.com",
